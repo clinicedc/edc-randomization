aecbec50ad372c3b488d9efe60327ea8b34ee96e
erikvw <ew2789@gmail.com>
2020-02-04 10:41:07 +0300

    allow assignment map customization using settings attr

--- edc_randomization/randomizers.py
+++ edc_randomization/randomizers.py
@@ -1,6 +1,7 @@
+from django.conf import settings
+
 from .randomizer import Randomizer
 from .site_randomizers import site_randomizers
-from django.conf import settings
 
 if getattr(settings, "EDC_RANDOMIZATION_REGISTER_DEFAULT_RANDOMIZER", True):
     site_randomizers.register(Randomizer)
