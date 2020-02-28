6f3e4b37cba2ac0483e64f99995a5c2338ba40af
erikvw <ew2789@gmail.com>
2020-02-28 16:21:37 -0600

    randomizer

--- edc_randomization/randomizer.py
+++ edc_randomization/randomizer.py
@@ -47,13 +47,12 @@ class Randomizer:
 
     name = "default"
     model = "edc_randomization.randomizationlist"
-
     assignment_map = DEFAULT_ASSIGNMENT_MAP
     file_name = "randomization_list.csv"
     is_blinded_trial = True
 
     def __init__(
-        self, subject_identifier=None, report_datetime=None, site=None, user=None
+            self, subject_identifier=None, report_datetime=None, site=None, user=None
     ):
         self._model_obj = None
         self._registered_subject = None
@@ -128,11 +127,11 @@ class Randomizer:
             except ObjectDoesNotExist:
                 self._model_obj = (
                     self.model_cls()
-                    .objects.filter(
+                        .objects.filter(
                         subject_identifier__isnull=True, site_name=self.site.name
                     )
-                    .order_by("sid")
-                    .first()
+                        .order_by("sid")
+                        .first()
                 )
                 if not self._model_obj:
                     raise AllocationError(
@@ -151,12 +150,12 @@ class Randomizer:
 
     def randomize(self):
         if any(
-            [
-                not self.subject_identifier,
-                not self.allocated_datetime,
-                not self.user,
-                not self.site,
-            ]
+                [
+                    not self.subject_identifier,
+                    not self.allocated_datetime,
+                    not self.user,
+                    not self.site,
+                ]
         ):
             dct = dict(
                 subject_identifier=self.subject_identifier,
