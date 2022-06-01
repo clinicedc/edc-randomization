d9de439343e0b4a7008809e1d2f8530ec29685d2
erikvw <ew2789@gmail.com>
2022-06-01 18:25:44 -0500

    lint

--- edc_randomization/tests/tests/test_auths.py
+++ edc_randomization/tests/tests/test_auths.py
@@ -1,6 +1,6 @@
 from importlib import import_module
 
-from django.test import TestCase, override_settings, tag
+from django.test import TestCase, override_settings
 from edc_auth.auth_updater import AuthUpdater
 from edc_auth.site_auths import site_auths
 
@@ -14,8 +14,3 @@ class TestAuths(TestCase):
         site_auths.initialize()
         import_module("edc_randomization.auths")
         AuthUpdater(verbose=False)
-        # site_auths.initialize()
-        # import_module("edc_navbar.auths")
-        # import_module("edc_dashboard.auths")
-        # import_module("edc_review_dashboard.auths")
-        # import_module("edc_randomization.auths")
